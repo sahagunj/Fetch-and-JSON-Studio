a65e2e0c2fd1c3ee64ebeb851608d237e45281e4
Jessica Sahagun <jessicasahagun@Jessicas-Air.lan>
2022-03-13 13:20:27 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 // TODO: add code here
+window.addEventListener("load", function () {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) 
+    {
+        response.json().then(function (json) {
+            let numberOfAstronauts = document.getElementById("numberOfAstronauts");
+            numberOfAstronauts.innerHTML = `Astronaut count: ${json.length}`
+
+            let div = document.getElementById("container");
+            for(const key of json) {
+                let color = "red";
+                if (key.active == true){
+                    color = "green"
+                };
+
+                div.innerHTML +=
+                `<div class="astronaut">
+                <div class="bio">
+                <h3>${key.firstName} ${key.lastName}</h3>
+                <ul>
+                <li> Hours in Space: ${key.hoursInSpace}</li>
+                <li style="color:${color}">Active: ${key.active}</li>
+                <li> Skills: ${key.skills}
+                </ul>
+                </div>
+                <img class="avatar" src="${key.picture}">
+                </div>
+                `
+            };
+        });
+    });
+});
